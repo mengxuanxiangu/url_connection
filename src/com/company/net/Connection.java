73301b73a23e92adca6dd38728d7a697cb6531b4
mengxuanxiangu <houjinchao@gmail.com>
2016-08-23 11:58:01 +0800

    back up

--- src/com/company/net/Connection.java
+++ src/com/company/net/Connection.java
@@ -1,15 +1,24 @@
 package com.company.net;
 
+import com.company.work.TimeData;
+
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 /**
  * Created by houjinchao on 16/8/21.
  */
 public class Connection {
 
-    public void httpUrlConnection() {
+    public boolean httpUrlConnection(byte[] data, TimeData time) {
+
+        // TODO:: tobe delete
+        time.setStart_time("aaa");
+        time.setEnd_time("bbb");
+        return true;
+        /*
         try {
             String pathUrl = "http://10.189.196.254:7001/trkproxy/trkPoint?socolimei=12345678901234";
             //String pathUrl = "http:://www.baidu.com";
@@ -40,19 +49,23 @@ public class Connection {
 
             if (res_p_code == HttpURLConnection.HTTP_OK) {
                 System.out.println("success");
+                //TODO::judge and get response data @yali
+                return true;
             } else {
                 System.out.println("faild" + res_p_code);
+                return false;
             }
-
-
-
-
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+        */
     }
 
-    public void httpUrlConnection_pb() {
+    public boolean httpUrlConnection_pb(byte[] data, ArrayList<String> list) {
+        //TODO: to be delete
+        list.add("ccc");
+        return true;
+        /*
         try {
             String pathUrl = "http://10.189.200.68:7001/trkproxy/trkPoint?imei=12345678901234&id=1461726264875_1461726270875";
             //String pathUrl = "http:://www.baidu.com";
@@ -82,15 +95,23 @@ public class Connection {
 
             if (res_p_code == HttpURLConnection.HTTP_OK) {
                 System.out.println("success");
+                //TODO judge and get response data @yali
+                return true;
             } else {
                 System.out.println("faild" + res_p_code);
+                return false;
             }
-
-
-
 
         } catch (Exception ex) {
             ex.printStackTrace();
+            return false;
         }
+        */
+    }
+
+    public boolean httpUrlConnection_pic(byte[] data) {
+        //TODO: to be delete
+        return true;
+        //TODO: add upload code @yali
     }
 }
